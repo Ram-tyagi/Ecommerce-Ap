b62baa3d9bbcc464d2319f9e5a55ba5d5dfebf9a
Ram-tyagi <ramtyagi04@gmail.com>
2023-08-11 03:14:48 +0530

    Product review page

--- src/App.js
+++ src/App.js
@@ -7,10 +7,44 @@ import  Footer  from "./Components/Layouts/Footer";
 import CartProvider from "./Components/Store/CartProvider";
 import About from "./Components/About/About";
 import Home from "./Components/Home/Home";
+import ContactUs from "./Components/ContactUs/ContactUs";
+import Product from "./Components/Product";
+const productsArrs = [
+  {
+    title: "Colors",
+    price: 100,
+    imageUrl: [
+      "https://prasadyash2411.github.io/ecom-website/img/Album%201.png",
+      "https://prasadyash2411.github.io/ecom-website/img/Album%202.png",
+    ],
+    des: "Lorem ipsum carrots enhanced rebates. Excellent sayings of a man of sorrows",
+  },
+
+  {
+    title: "Black and white Colors",
+    price: 50,
+    imageUrl: ["https://prasadyash2411.github.io/ecom-website/img/Album%202.png"],
+    des: "Lorem ipsum carrots enhanced rebates. Excellent sayings of a man of sorrows",
+  },
+
+  {
+    title: "Yellow and Black Colors",
+    price: 70,
+    imageUrl: ["https://prasadyash2411.github.io/ecom-website/img/Album%203.png"],
+    des: "Lorem ipsum carrots enhanced rebates. Excellent sayings of a man of sorrows",
+  },
+
+  {
+    title: "Blue Color",
+    price: 100,
+    imageUrl: ["https://prasadyash2411.github.io/ecom-website/img/Album%204.png"],
+    des: "Lorem ipsum carrots enhanced rebates. Excellent sayings of a man of sorrows",
+  },
+];
 
 function App() {
  
-    const [storeVisible, setStoreVisible] = useState(false);
+    const [storeVisible, setStoreVisible] = useState(true);
     const [cartVisible, setCartVisible] = useState(false);
   
     const visibleStoreHandler = () => {
@@ -35,7 +69,9 @@ function App() {
         <Routes>
           <Route path="/"  element={ <Home />}/>
         <Route path="/about"  element={ <About />}/>
-        <Route path="/store"  element= {storeVisible && <Store />}/>
+        <Route path="/store"  element= {storeVisible && <Store  productsArr={productsArrs}/>}/>
+        <Route path="/contactus"  element={ <ContactUs />}/>
+        <Route path="/store/:productId"  element={<Product productsArr={productsArrs} />}/>
         </Routes>
         {cartVisible && <Cart />}
         <Footer />
--- src/Components/Store.js
+++ src/Components/Store.js
@@ -2,38 +2,11 @@ import React,{useContext} from "react";
 import { Card, Container, Row, Button, Col } from "react-bootstrap";
 import CartContext from "./Store/cart-context";
 import classes from "./Store.module.css";
+import Product from "./Product";
+import { Link} from "react-router-dom";
 
-const productsArr = [
-    {
-      title: "Colors",
-      price: 100,
-      imageUrl:
-        "https://prasadyash2411.github.io/ecom-website/img/Album%201.png",
-    },
 
-    {
-      title: "Black and white Colors",
-      price: 50,
-      imageUrl:
-        "https://prasadyash2411.github.io/ecom-website/img/Album%202.png",
-    },
-
-    {
-      title: "Yellow and Black Colors",
-      price: 70,
-      imageUrl:
-        "https://prasadyash2411.github.io/ecom-website/img/Album%203.png",
-    },
-
-    {
-      title: "Blue Color",
-      price: 100,
-      imageUrl:
-        "https://prasadyash2411.github.io/ecom-website/img/Album%204.png",
-    },
-  ];
-
-const Store = () => {
+const Store = (props) => {
     const cartCtx = useContext(CartContext);
 
     const clickHandler = (event) => {
@@ -46,22 +19,37 @@ const Store = () => {
         if (copy.length > 0) {
           cartCtx.quantityChange(copy[0].id);
         } else {
-          const ele = { id: eleIdx, ...productsArr[eleIdx], quantity: 1 };
+          const ele = { id: eleIdx, ...props.productsArr[eleIdx], quantity: 1 };
           cartCtx.addCartItem({ ...ele });
         }
       } else {
-        const ele = { id: eleIdx, ...productsArr[eleIdx], quantity: 1 };
+        const ele = { id: eleIdx, ...props.productsArr[eleIdx], quantity: 1 };
         cartCtx.addCartItem({ ...ele });
       }
     };
-
+  const p="/store/";
   return (
     <Container className={classes.card}>
        <Row xs={1} md={2} className="g-8">
-      {productsArr.map((item, idx) => (
+      {props.productsArr.map((item, idx) => (
         <Col key={idx}>
-          <Card id={idx} style={{width: '20rem'}} className="mt-3">
-            <Card.Title style={{textAlign: 'center', paddingTop: '0.5rem'}}>{item.title}</Card.Title>
+            <Card
+                id={idx}
+                style={{ width: "20rem", backgroundColor: "#e4f2f2" }}
+                className="mt-3"
+              >
+                <Link to={p + item.title}>
+                  <Card.Title
+                    style={{ textAlign: "center", paddingTop: "0.5rem" }}
+                  >
+                    {item.title}
+                  </Card.Title>
+                  <Card.Img
+                    variant="top"
+                    src={item.imageUrl[0]}
+                    className={classes.img}
+                  />
+                </Link>
             <Card.Img variant="top" src={item.imageUrl} className={classes.img}/>
             <Card.Body className={classes.body}>
               <Card.Text>${item.price}</Card.Text>
