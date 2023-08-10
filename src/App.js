df4dbafcd6408c4be83e405b90140cddbd0de1d4
Ram-tyagi <ramtyagi04@gmail.com>
2023-08-11 02:41:47 +0530

    home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
+import { Route, Routes } from "react-router-dom";
 import React, { useState } from "react";
 import Cart from "./Components/Cart/Cart";
 import Store from "./Components/Store";
 import Header from "./Components/Layouts/Header";
 import  Footer  from "./Components/Layouts/Footer";
 import CartProvider from "./Components/Store/CartProvider";
+import About from "./Components/About/About";
+import Home from "./Components/Home/Home";
 
 function App() {
  
@@ -26,11 +28,15 @@ function App() {
     return (
       <CartProvider>
       <div>
-        <Header
+      <Header
           onClickStore={visibleStoreHandler}
           onClickCart={visibleCartHandler}
         />
-        {storeVisible && <Store />}
+        <Routes>
+          <Route path="/"  element={ <Home />}/>
+        <Route path="/about"  element={ <About />}/>
+        <Route path="/store"  element= {storeVisible && <Store />}/>
+        </Routes>
         {cartVisible && <Cart />}
         <Footer />
       </div>
