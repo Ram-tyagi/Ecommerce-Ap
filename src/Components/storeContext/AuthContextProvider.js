3807bd2df8b997613612d6c76e5cc56ab78d7205
Ram-tyagi <ramtyagi04@gmail.com>
2023-08-14 03:16:40 +0530

    making cart user specific

--- src/Components/storeContext/AuthContextProvider.js
+++ src/Components/storeContext/AuthContextProvider.js
@@ -3,25 +3,31 @@ import AuthContext from "./auth-context";
 
 const AuthContextProvider = (props) => {
   const [token, setToken] = useState(null);
-
+  const [userEmail, setUserEmail] = useState(null);
   if (token === null && localStorage.length !== 0) {
     setToken(localStorage["user"]);
+    setUserEmail(localStorage["userEmail"]);
   }
 
   const userLoggedIn = !!token;
 
-  const loginHandler = (tokenId) => {
+  const loginHandler = (tokenId,email) => {
     setToken(tokenId);
+    setUserEmail(email);
     localStorage.setItem("user", tokenId);
+    localStorage.setItem("email", email);
   };
 
   const logoutHandler = () => {
     setToken(null);
+    setUserEmail(null);
     localStorage.removeItem("user");
+    localStorage.removeItem("userEmail");
   };
 
   const authContext = {
     token: token,
+    userEmail: userEmail,
     isLoggedIn: userLoggedIn,
     login: loginHandler,
     logout: logoutHandler,
